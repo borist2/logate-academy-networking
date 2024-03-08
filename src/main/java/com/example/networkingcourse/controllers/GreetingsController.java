38291463730b92ee4cb33134f1cb7adc758abc40
boristuponja <boris.tuponja@logate.com>
2024-03-08 12:34:21 +0100

    Basic greeting with custom message

--- src/main/java/com/example/networkingcourse/controllers/GreetingsController.java
+++ src/main/java/com/example/networkingcourse/controllers/GreetingsController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/greetings")
 public class GreetingsController
 {
     @GetMapping
-    public String greeting(Model model) {
-        model.addAttribute("message", "Hello, Spring MVC");
+    public String greeting(@RequestParam("message") String message, Model model)
+    {
+        model.addAttribute("message", "Custom message: " + message);
         return "greeting"; // Name of the view (greeting.html)
     }
 }
