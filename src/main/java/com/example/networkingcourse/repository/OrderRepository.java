8281aa6dc9c296db7e5b365819f2959a06e5d5f1
boristuponja <boris.tuponja@logate.com>
2024-07-29 19:54:16 +0200

    Showcase Entity graph

--- src/main/java/com/example/networkingcourse/repository/OrderRepository.java
+++ src/main/java/com/example/networkingcourse/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.example.networkingcourse.repository;
 
 import com.example.networkingcourse.model.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ public interface OrderRepository extends Repository<Order, Integer>
 {
     Order save(Order order);
 
+    @EntityGraph(attributePaths = "items")
     List<Order> findAll();
 }
