8ed4bad6eb846b0eac573ad744cb517a17560777
boristuponja <boris.tuponja@logate.com>
2024-07-30 17:13:48 +0200

    Send message to topic

--- src/main/java/com/example/networkingcourse/controllers/StompController.java
+++ src/main/java/com/example/networkingcourse/controllers/StompController.java
@@ -1,18 +1,27 @@
 package com.example.networkingcourse.controllers;
 
 import com.example.networkingcourse.dto.ChatMessageDTO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/chats")
 public class StompController
 {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/chat.sendMessage")
     public void sendMessage(@Payload ChatMessageDTO chatMessage)
     {
@@ -40,5 +49,12 @@ public class StompController
         return "Response on subscribe";
     }
 
+    @PostMapping
+    public ResponseEntity<Void> sendMessageToTopic(@RequestBody ChatMessageDTO chatMessageDTO)
+    {
+        simpMessagingTemplate.convertAndSend("/topic/new-messages", chatMessageDTO);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
